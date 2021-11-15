239c1b67e3bbe22137d4b27089b6a38250c6d60c
JvWelzenis <83783857+JvWelzenis@users.noreply.github.com>
2021-11-15 14:33:56 +0100

    Add files via upload

---
+++ if_else_statements/script.js
@@ -0,0 +1,22 @@
+const myAge = 40;
+if (myAge >=18) {
+    console.log('please come in');
+} else {
+    console.log('please stay outside');
+}
+
+
+const isFemale = true;
+
+if (isFemale) {
+    console.log("Yes, true!");
+} else  {
+    console.log("Nope, false!");
+}
+
+const driverStatement = "bob";
+if (driverStatement == "bob") {
+    console.log("you're allowed to drive");
+} else {
+    console.log("you're not allowed to drive");
+}
